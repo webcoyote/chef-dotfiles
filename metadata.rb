69101513de1035ad8637e1bd680f420714038cbb
Patrick Wyatt <pat@codeofhonor.com>
2014-04-15 12:28:50 -0700

    added name to metadata

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,7 @@
+name             "chef-dotfiles"
 maintainer       "Patrick Wyatt"
 maintainer_email "pat@codeofhonor.com"
 license          "MIT"
 description      "Installs and configures dotfiles"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.1"
+version          "0.1.2"
